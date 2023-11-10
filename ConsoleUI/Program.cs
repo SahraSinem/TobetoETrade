99a26adec18328544455007b2cd0931db8a680f7
SahraSinem <s.sinem_can@hotmail.com>
2023-11-10 23:28:24 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Sıfırdan proje (solution) oluşturunuz-->TobetoETrade 
+//Product sınıfını katmanlarında işletiniz. 
+//Product -->Id,Name,UnitPrice
+//program.cs de mutlaka veri girişleri yapınız.
+//tüm pairler bitmeden sonraki aşamaya geçmek yok :)
+//tüm pairler bitince 10 dk mola.
+//İkinci aşama. Önceki derste (çarşamba) oluşturduğumuz kodlama.io varlıklarını (entity) 
+//TobetoCourseAcademy projesine uygulayınız. 
+//program.cs de mutlaka veri girişleri yapınız.
+
+using Business.Concretes;
+using Entities.Concretes;
+
+Product product = new Product();
+product.Name = "Laptop";
+product.Id = 1;
+product.UnitPrice = 40.000;
+
+ProductManager productManager = new ProductManager();
+productManager.Add(product);
+
+
---
+++ DataAccess/Concretes/AdoNetProductDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concretes;
+
+namespace DataAccess.Concretes
+{
+    public class AdoNetProductDal
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Adonet ile ürün eklendi.");
+        }
+    }
+}
